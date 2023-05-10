94cab6ce31ada7b6fc4c2548614511cc6db6c9dc
Fuad Memmedov <m.fuad@code.edu.az>
2023-05-10 20:53:49 +0400

    Some editing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,9 +3,6 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
-//
-// Scripts
-// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -32,3 +29,31 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+const btnElement =document.querySelectorAll(".btn");
+const itemEls =document.querySelectorAll(".item");
+
+var tempActive = document.querySelector(".active");
+
+btnElement.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        //Active status nav menu
+        tempActive.classList.remove("active")
+        btn.classList.add("active")
+        tempActive = btn;
+
+        const name = e.target.dataset.name;
+        itemEls.forEach((itemEl)=>{
+            if(name==="all"){
+                itemEl.style.display = "block"
+            }else{
+                if(itemEl.classList.contains(name)){
+                    itemEl.style.display = "block"
+                }else{
+                    itemEl.style.display = "none";
+                }
+            }
+        })
+    })
+})
