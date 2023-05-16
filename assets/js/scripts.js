f965d86389d0af96f10378939f057c0af2f14817
Fuad Memmedov <m.fuad@code.edu.az>
2023-05-17 01:32:23 +0400

    some fixing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,8 @@
-/*!
-* Start Bootstrap - Resume v7.0.4 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
+/*! Start Bootstrap - Resume v7.0.4 (https://startbootstrap.com/theme/resume) Copyright 2013-2021 Start Bootstrap
+Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE) */
 
 window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
+    
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
@@ -15,11 +11,8 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     };
 
-    // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
+    const responsiveNavItems = [].slice.call( document.querySelectorAll('#navbarResponsive .nav-link'));
     responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
@@ -27,33 +20,27 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
 });
 
 
-const btnElement =document.querySelectorAll(".btn");
-const itemEls =document.querySelectorAll(".item");
-
+const btnElement =document.querySelectorAll(".button");
 var tempActive = document.querySelector(".active");
 
 btnElement.forEach((btn)=>{
     btn.addEventListener("click",(e)=>{
-        //Active status nav menu
         tempActive.classList.remove("active")
         btn.classList.add("active")
         tempActive = btn;
-
-        const name = e.target.dataset.name;
-        itemEls.forEach((itemEl)=>{
-            if(name==="all"){
-                itemEl.style.display = "block"
-            }else{
-                if(itemEl.classList.contains(name)){
-                    itemEl.style.display = "block"
-                }else{
-                    itemEl.style.display = "none";
-                }
-            }
-        })
     })
 })
+
+let $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows'
+    });
+
+    $('.filters-button-group').on( 'click', 'button', function() {
+    let filterValue = $( this ).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+    });
+  
